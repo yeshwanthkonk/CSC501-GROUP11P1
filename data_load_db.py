e18f1daaeabf45ea29ae48307e3d0a3216b07572
Yeshwanth Konka <yeshwanthkonka@yahoo.com>
2024-10-06 18:43:51 -0700

    uncommenting the changes

--- data_load_db.py
+++ data_load_db.py
@@ -11,12 +11,12 @@ db_schema = 'GROUP11P1'
 files = ['./datafiles/Badges.csv', './datafiles/Comments.csv','./datafiles/PostTypes.csv','./datafiles/Posts.csv','./datafiles/PostTags.csv','./datafiles/Tags.csv','./datafiles/Users.csv','./datafiles/Votes.csv']
 
 table_etl_meta = {
-    # "Tags": dict(source_file="Tags", dest_columns=["tag_id", "tag_name"], source_columns=["Id", "TagName"]),
-    # "Users": dict(source_file="Users", dest_columns=["id", "reputation"], source_columns=["Id", "Reputation"]),
-    # "PostTypes": dict(source_file="PostTypes", dest_columns=["id", "type_title"], source_columns=["Id", "Type_name"]),
-    # "Badges": dict(source_file="Badges", dest_columns=["id", "badge_title", "badge_class"], source_columns=["Id", "Name", "Class"]),
-    # "Posts": dict(source_file="Posts", dest_columns=["id", "created_date", "post_score", "created_by", "post_type_id"], source_columns=["Id", "CreationDate", "Score", "OwnerUserId", "PostTypeId"]),
-    # "Votes": dict(source_file="Votes", dest_columns=["vote_id", "post_id"], source_columns=["Id", "PostId"]),
+    "Tags": dict(source_file="Tags", dest_columns=["tag_id", "tag_name"], source_columns=["Id", "TagName"]),
+    "Users": dict(source_file="Users", dest_columns=["id", "reputation"], source_columns=["Id", "Reputation"]),
+    "PostTypes": dict(source_file="PostTypes", dest_columns=["id", "type_title"], source_columns=["Id", "Type_name"]),
+    "Badges": dict(source_file="Badges", dest_columns=["id", "badge_title", "badge_class"], source_columns=["Id", "Name", "Class"]),
+    "Posts": dict(source_file="Posts", dest_columns=["id", "created_date", "post_score", "created_by", "post_type_id"], source_columns=["Id", "CreationDate", "Score", "OwnerUserId", "PostTypeId"]),
+    "Votes": dict(source_file="Votes", dest_columns=["vote_id", "post_id"], source_columns=["Id", "PostId"]),
     "Comments": dict(source_file="Comments", dest_columns=["id", "user_id", "created_at", "score"], source_columns=["Id", "UserId", "CreationDate", "Score"]),
     "BadgeEarns": dict(source_file="Badges", dest_columns=["user_id", "badge_id", "allowted_date"], source_columns=["UserId", "Id", "Date"]),
     "PostTags": dict(source_file="PostTags", dest_columns=["tag_id", "post_id"], source_columns=["Id_y", "Id_x"])
